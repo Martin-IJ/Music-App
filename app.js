2e921965a7856d4a64a9eecf3c707de2dff5e16e
Martin-IJ <martinohez@gmail.com>
2022-10-05 15:01:52 +0100

    saved folder

---
+++ app.js
@@ -0,0 +1,39 @@
+const music = [
+    {
+        title: "2Face Idibia - African Queen",
+        name: "2Face",
+        image: "./music/2face/2face African QUEEN.png",
+        song: "./music/2face/2Face Idibia - African Queen.mp3",
+    },
+    {
+        title: "Pretty Little Fears ft J-Cole",
+        name: "6Lack",
+        image: "./music/6lack/pretyy little fears.png",
+        song: "./music/6lack/6LACK_-_Pretty_Little_Fears_ft_J_Cole.mp3",
+    },
+    {
+        title: "Twice as Tall",
+        name: "Burna Boy",
+        image: "./music/Burna boy - Twice as Tall/Burna-Boy-Level-Up-Twice-As-Tall-artwork.png",
+        song: "./music/Burna boy - Twice as Tall/01-Burna-Boy-Level-Up-Twice-As-Tall-ft-Youssou-N-Dour-360media.com_.ng_.mp3",
+    },
+    {
+        title: "Middle Child",
+        name: "J-Cole",
+        image: "./music/J cole - middle child/J._Cole_-_Middle_Child.png",
+        song: "./music/J cole - middle child/[Waploaded]_J._Cole_-_Middle_Child-1548343409.mp3",
+    },
+    {
+        title: "Omah Lay - Godly",
+        name: "Omah Lay",
+        image: "./music/Omah lay - Godly/Omah-Lay-What-Have-We-Done-EP.jpg",
+        song: "./music/Omah lay - Godly/03 Omah Lay - Godly (NetNaija.com).mp3",
+    },
+]
+
+const musicImg = document.querySelector(".music-img")
+const musicTitle = document.querySelector(".music-title")
+const musician = document.querySelector(".musician")
+const prev = document.querySelector(".prev")
+const play = document.querySelector(".play")
+const next = document.querySelector(".next")
