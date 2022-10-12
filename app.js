f98f4460688b9fd4158769e66b4fafa09e6a5345
Martin-IJ <martinohez@gmail.com>
2022-10-12 13:26:08 +0100

    last commit

--- app.js
+++ app.js
@@ -1,39 +1,57 @@
-const music = [
-    {
-        title: "2Face Idibia - African Queen",
-        name: "2Face",
-        image: "./music/2face/2face African QUEEN.png",
-        song: "./music/2face/2Face Idibia - African Queen.mp3",
-    },
-    {
-        title: "Pretty Little Fears ft J-Cole",
-        name: "6Lack",
-        image: "./music/6lack/pretyy little fears.png",
-        song: "./music/6lack/6LACK_-_Pretty_Little_Fears_ft_J_Cole.mp3",
-    },
-    {
-        title: "Twice as Tall",
-        name: "Burna Boy",
-        image: "./music/Burna boy - Twice as Tall/Burna-Boy-Level-Up-Twice-As-Tall-artwork.png",
-        song: "./music/Burna boy - Twice as Tall/01-Burna-Boy-Level-Up-Twice-As-Tall-ft-Youssou-N-Dour-360media.com_.ng_.mp3",
-    },
-    {
-        title: "Middle Child",
-        name: "J-Cole",
-        image: "./music/J cole - middle child/J._Cole_-_Middle_Child.png",
-        song: "./music/J cole - middle child/[Waploaded]_J._Cole_-_Middle_Child-1548343409.mp3",
-    },
-    {
-        title: "Omah Lay - Godly",
-        name: "Omah Lay",
-        image: "./music/Omah lay - Godly/Omah-Lay-What-Have-We-Done-EP.jpg",
-        song: "./music/Omah lay - Godly/03 Omah Lay - Godly (NetNaija.com).mp3",
-    },
-]
-
 const musicImg = document.querySelector(".music-img")
 const musicTitle = document.querySelector(".music-title")
 const musician = document.querySelector(".musician")
 const prev = document.querySelector(".prev")
 const play = document.querySelector(".play")
+const playCont = document.querySelector(".player")
 const next = document.querySelector(".next")
+const pause = document.getElementById("pause");
+const audioTag = document.createElement('audio')
+
+let currentPosition = 0;
+
+let isPlaying = false;
+
+function loadPlaylist(p) {
+   musicImg.src = musicList[p].image;
+   musicTitle.innerHTML = musicList[p].title;
+   musician.innerHTML = musicList[p].name
+    audioTag.src = musicList[p].audio;
+    audioTag.load();
+    audioTag.addEventListener("ended", nextBtn)
+    
+}
+
+loadPlaylist(currentPosition)
+playCont.addEventListener('click',()=>{
+    isPlaying ? noPlay(): showPlay();
+    
+})
+function  showPlay() {
+    isPlaying = true
+   playCont.innerHTML = `  
+   <i class="fa-solid fa-pause play"></i>`;
+   audioTag.play()
+}
+function  noPlay() {
+    isPlaying = false
+   playCont.innerHTML = `  
+   <i class="fa-solid fa-play play"></i>`;
+   audioTag.pause()
+}
+function nextBtn() {
+    if (currentPosition < musicList.length - 1) {
+        currentPosition += 1;   
+    } else{
+        currentPosition = 0
+    }
+    loadPlaylist(currentPosition)
+    showPlay() 
+}
+function prevBtn() {
+    currentPosition > 0 ? currentPosition -= 1: currentPosition = musicList.length -1;      
+    loadPlaylist(currentPosition)
+    showPlay() 
+}
+next.addEventListener('click', nextBtn)
+prev.addEventListener('click', prevBtn)
---
+++ json.js
@@ -0,0 +1,32 @@
+const musicList = [
+    {
+        title: "2Face Idibia - African Queen",
+        name: "2Face",
+        image: "./music/2face/2face African QUEEN.png",
+        audio: "./music/2face/2Face Idibia - African Queen.mp3",
+    },
+    {
+        title: "Pretty Little Fears ft J-Cole",
+        name: "6Lack",
+        image: "./music/6lack/pretyy little fears.png",
+        audio: "./music/6lack/6LACK_-_Pretty_Little_Fears_ft_J_Cole.mp3",
+    },
+    {
+        title: "Twice as Tall",
+        name: "Burna Boy",
+        image: "./music/Burna boy - Twice as Tall/Burna-Boy-Level-Up-Twice-As-Tall-artwork.png",
+        audio: "./music/Burna boy - Twice as Tall/01-Burna-Boy-Level-Up-Twice-As-Tall-ft-Youssou-N-Dour-360media.com_.ng_.mp3",
+    },
+    {
+        title: "Middle Child",
+        name: "J-Cole",
+        image: "./music/J cole - middle child/J._Cole_-_Middle_Child.png",
+        audio: "./music/J cole - middle child/[Waploaded]_J._Cole_-_Middle_Child-1548343409.mp3",
+    },
+    {
+        title: "Omah Lay - Godly",
+        name: "Omah Lay",
+        image: "./music/Omah lay - Godly/Omah-Lay-What-Have-We-Done-EP.jpg",
+        audio: "./music/Omah lay - Godly/03 Omah Lay - Godly (NetNaija.com).mp3",
+    },
+]
